0b5e5a0f241f9478702309b64717f899ffa52e29
Mario Valente <mario.valente@atlastechnol.com>
2022-08-19 10:00:17 -0300

    Whith this update it is possible to test the componets of the report page

--- cypress/Pages/whistleblowerPage.js
+++ cypress/Pages/whistleblowerPage.js
@@ -1,20 +1,22 @@
 
-const home = cy.visit("https://fatalmodel.com/") 
+const home = "https://fatalmodel.com/"
+
+const viewportWidth = 1440
+const viewportHeight = 720
 
 class WhistleblowerPageResources{
+//Define a Viewport Desktop
+setDesktopViewport() {
+Cypress.config('viewportWidth', viewportWidth)
+Cypress.config('viewportHeight', viewportHeight)
+  }
 
 //Entrar na página home
 visitHome(){
     cy.visit(home)
 }
 
-//Entrar na página de denuncias
-visitWistleblower(){
-   // descobrir uma maneira de pegar a outra ABA criada 
-  
 
-    
-}
 
 //Acessa a página Home e limpa o Session Storage / Faz aparecer sempre a modal +18 terms e Cookies
 visitHomeAndClearSessionStorage() {
@@ -25,8 +27,8 @@ visitHomeAndClearSessionStorage() {
     })
   }
   //Identifica e clica no botão da modal +18 terms e Cookies
-  agreeModalTerms() {
-    cy.contains('Concordo').click()
+agreeModalTerms() {
+  cy.contains('Concordo').click()
   }
 
 //Aperta o botão continuar da página de denuncias
@@ -48,5 +50,43 @@ leaveSameTabe(){
 visitAdProfile(){
   cy.visit('https://fatalmodel.com/1362118/laura-fernandes-1362118')
 }
-    
+
+//checa se o titulo é visivel 
+CheckTitleIsVisible(){
+  cy.get('.denounce__title > .inner-text').should ('be.visible')
+  cy.contains('ESCOLHA UM MOTIVO PARA A DENÚNCIA').should('be.visible')
 }
+
+//checa se o subtitulo é visivel
+checkSubTitleIsVisible(){
+  cy.get('.denounce__subtitle > .inner-text').should('be.visible')
+  cy.contains('Selecione abaixo o motivo pelo qual você está denunciando este perfil.').should('be.visible')
+}
+
+//Check button Irregularidade no perfil is visible
+checkButtonIrregularidadeIsVisible(){
+  cy.get('.denounce__types > :nth-child(1)').should ('be.visible')
+    }
+
+
+
+//Check button Extorsão e golpe do pix is visible
+checkButtonGolpepixIsVisible(){
+cy.get('.denounce__types > :nth-child(2)').should ('be.visible')
+}
+
+
+
+//Check button Gênero incorreto is visible
+checkButtonGeneroIsVisible(){
+cy.get('.denounce__types > :nth-child(3)').should ('be.visible')
+}
+
+
+
+//Check content is visible
+checkContentIsVisible(){
+cy.get('.denounce__content').should ('be.visible')
+}
+}
+export default new WhistleblowerPageResources;
--- cypress/e2e/WhistleblowerPageResources.cy.js
+++ cypress/e2e/WhistleblowerPageResources.cy.js
@@ -1,76 +1,49 @@
 /// <reference types="cypress" />
-//import  WhistleblowerPageResources from '../Pages/whistleblowerPage'
+import whistleblowerPage from '../Pages/whistleblowerPage';
+import  WhistleblowerPageResources from '../Pages/whistleblowerPage'
 
 describe("Whistleblower Page Test", () => {
-   
-    it ("Choose a reason to report", () => {
-       cy.visit('https://fatalmodel.com/', {
-        onBeforeLoad: win => {
-          win.sessionStorage.clear()
-        }
-      })
-       cy.contains('Concordo').click()
-       cy.visit('https://fatalmodel.com/1362118/laura-fernandes-1362118')
-       cy.contains('a', 'Denunciar perfil ').invoke('removeAttr','target').click()
-      
-       cy.contains('button', 'Continuar').click()
-       cy.get('.denounce__form > [data-v-4c838268=""]').click().type('a@a.com')
-       cy.contains('button', 'Continuar').click()
-
-    
-
-      
+    WhistleblowerPageResources.setDesktopViewport();
+  
+ it('Agree modal +18 terms and Cookies', () => {
+     WhistleblowerPageResources.visitHomeAndClearSessionStorage();
+     WhistleblowerPageResources.agreeModalTerms();
 
     })
-
+    it('Check title is visible', () =>{
+         WhistleblowerPageResources.visitAdProfile();
+         WhistleblowerPageResources.agreeModalTerms();
+         WhistleblowerPageResources.leaveSameTabe();
+         WhistleblowerPageResources.clickContinue();
+         WhistleblowerPageResources.adEmail();
+         WhistleblowerPageResources.clickContinue();
+  
+    })
     it ('Check title is visible', () => {
-        cy.get('.denounce__title > .inner-text').should ('be.visible')
-        cy.contains('ESCOLHA UM MOTIVO PARA A DENÚNCIA')
+        WhistleblowerPageResources.CheckTitleIsVisible();
     })
 
     it('Check subtitle is visible', () => {
-        cy.get('.denounce__subtitle > .inner-text').should('be.visible')
-        cy.contains('Selecione abaixo o motivo pelo qual você está denunciando este perfil.').should('be.visible')
-        
-    })
-
+        WhistleblowerPageResources.checkSubTitleIsVisible();
+         
+     })
     
-    it ('Check button Irregularidade no perfil is visible', () => {
-        cy.get('.denounce__types > :nth-child(1)').should ('be.visible')
+     it ('Check button Irregularidade no perfil is visible', () => {
+        WhistleblowerPageResources.checkButtonIrregularidadeIsVisible();
     })
-
+    
     it ('Check button Extorsão e golpe do pix is visible', () => {
-        cy.get('.denounce__types > :nth-child(2)').should ('be.visible')
-    })
-
-    it ('Check button Gênero incorreto is visible', () => {
-        cy.get('.denounce__types > :nth-child(3)').should ('be.visible')
-    })
+        WhistleblowerPageResources.checkButtonGolpepixIsVisible();
+     })
     
-    it ('Check content is visible', () => {
-        cy.get('.denounce__content > :nth-child(1)').should ('be.visible')
+     it ('Check button Gênero incorreto is visible', () => {
+        WhistleblowerPageResources.checkButtonGeneroIsVisible()
+      
+       
+     })
+     it ('Check content is visible', () => {
+        WhistleblowerPageResources.checkContentIsVisible();
     })
-
-
-
-
-    //it('Agree modal +18 terms and Cookies', () => {
-     // WhistleblowerPageResources.visitHomeAndClearSessionStorage();
-    // WhistleblowerPageResourcesa.agreeModalTerms();
-
-   // })
-
-   // it('Check title is visible', () =>{
-      //  WhistleblowerPageResources.visitProfile();
-      //  WhistleblowerPageResources.leaveSameTabe();
-      //  WhistleblowerPageResources.clickContinue();
-       // WhistleblowerPageResources.adEmail();
-      //  WhistleblowerPageResources.clickContinue();
-
-
-
-    //})
-   
 })
 
 
